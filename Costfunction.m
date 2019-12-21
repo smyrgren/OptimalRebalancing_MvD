c95217eda6f2f37388216c4ed50913f9a5e6c91f
smyrgren <Simon.myrgren@gmail.com>
2019-12-21 17:15:32 +0100

    Made cfMVT... object methods instead

--- Costfunction.m
+++ Costfunction.m
@@ -1,4 +1,5 @@
 classdef Costfunction < Portfolio & matlab.mixin.CustomDisplay
+
     properties
         funcName
         newWts
@@ -61,74 +62,74 @@ classdef Costfunction < Portfolio & matlab.mixin.CustomDisplay
             switch obj.funcName
                 case 'MV'
                     if ~getOptimal % Optimize with transaction costs
-                        [dw, ~, obj.exitFlag] = fmincon(@(dw)cfMV_wTC(dw, obj.curWts',  obj.expRts, ...
-                                            obj.tnsCst, obj.lambdaAbs, obj.covMtx), ...
+                        [dw, ~, obj.exitFlag] = fmincon(@(dw)cfMVT_wTC(obj, dw, obj.curWts', ...
+                                            zeros(numAssets,1), obj.expRts, ...
+                                            obj.tnsCst, obj.lambdaAbs, 0, obj.covMtx), ...
                                     dw0, A, b, Aeq, beq, lb, ub, nonlincons, options);
-                            
                         obj.newWts = obj.curWts + (dw(1:end/2) - dw(end/2+1:end))';
-                        obj.funcValCur = cfMV(obj.curWts', obj.expRts, obj.lambdaAbs, ...
-                                             obj.covMtx);
-                        obj.funcValNew = cfMV(obj.newWts', obj.expRts, obj.lambdaAbs, ...
-                                             obj.covMtx);
+                        obj.funcValCur = cfMVT(obj, obj.curWts', zeros(numAssets,1), obj.expRts, ...
+                                             obj.lambdaAbs, 0, obj.covMtx);
+                        obj.funcValNew = cfMVT(obj, obj.newWts', zeros(numAssets,1), obj.expRts, ...
+                                             obj.lambdaAbs, 0, obj.covMtx);
                         obj.paidTC = obj.tnsCst * abs(obj.curWts - obj.newWts)';
                     else % Optimize without transaction costs -> OPTIMAL ALLOCATION
-                        [dw, ~, obj.exitFlag] = fmincon(@(dw)cfMV_wTC(dw, obj.curWts', obj.expRts, ...
-                                            zeros(size(obj.tnsCst)), obj.lambdaAbs,  obj.covMtx), ...
-                                    dw0, A, b, Aeq, beq, lb, ub, nonlincons, options);
-                            
+                        [dw, ~, obj.exitFlag] = fmincon(@(dw)cfMVT_wTC(obj, dw, obj.curWts',  ...
+                                            zeros(numAssets,1), obj.expRts,  ...
+                                    zeros(size(obj.tnsCst)), obj.lambdaAbs, 0, obj.covMtx), dw0, ...
+                                     A, b, Aeq, beq, lb, ub, nonlincons, options);            
                         obj.optWts = obj.curWts + (dw(1:end/2) - dw(end/2+1:end))';
-                        obj.funcValCur = cfMV(obj.curWts',  obj.expRts, obj.lambdaAbs, ...
-                                             obj.covMtx);
-                        obj.funcValOpt = cfMV(obj.optWts',  obj.expRts, obj.lambdaAbs, ...
-                                             obj.covMtx);
+                        obj.funcValCur = cfMVT(obj, bj.curWts', zeros(numAssets,1), obj.expRts, ...
+                                             obj.lambdaAbs, 0, obj.covMtx);
+                        obj.funcValOpt = cfMVT(obj, obj.optWts', zeros(numAssets,1), obj.expRts, ...
+                                             obj.lambdaAbs, 0, obj.covMtx);
                     end
                 case 'MVT'
                     if ~getOptimal % Optimize with transaction costs
-                        [dw, ~, obj.exitFlag] = fmincon(@(dw)cfMVT_wTC(dw, obj.curWts', obj.benWts', obj.expRts, ...
-                                            obj.tnsCst, obj.lambdaAbs, obj.lambdaRel, obj.covMtx), ...
-                                    dw0, A, b, Aeq, beq, lb, ub, nonlincons, options);
-                            
+                        [dw, ~, obj.exitFlag] = fmincon(@(dw)cfMVT_wTC(obj, dw, obj.curWts', ...
+                                            obj.benWts', obj.expRts, obj.tnsCst, obj.lambdaAbs, ...
+                                    obj.lambdaRel, obj.covMtx), dw0, A, b, Aeq, beq, lb, ub, ...
+                                    nonlincons, options);
                         obj.newWts = obj.curWts + (dw(1:end/2) - dw(end/2+1:end))';
-                        obj.funcValCur = cfMVT(obj.curWts', obj.benWts', obj.expRts, obj.lambdaAbs, ...
-                                            obj.lambdaRel, obj.covMtx);
-                        obj.funcValNew = cfMVT(obj.newWts', obj.benWts', obj.expRts, obj.lambdaAbs, ...
-                                            obj.lambdaRel, obj.covMtx);
+                        obj.funcValCur = cfMVT(obj, obj.curWts', obj.benWts', obj.expRts,  ...
+                                            obj.lambdaAbs, obj.lambdaRel, obj.covMtx);
+                        obj.funcValNew = cfMVT(obj, obj.newWts', obj.benWts', obj.expRts,  ...
+                                            obj.lambdaAbs, obj.lambdaRel, obj.covMtx);
                         obj.paidTC = obj.tnsCst * abs(obj.curWts - obj.newWts)';
                     else % Optimize without transaction costs
-                        [dw, ~, obj.exitFlag] = fmincon(@(dw)cfMVT_wTC(dw, obj.curWts', obj.benWts', obj.expRts, ...
-                                            zeros(size(obj.tnsCst)), obj.lambdaAbs, obj.lambdaRel, obj.covMtx), ...
-                                    dw0, A, b, Aeq, beq, lb, ub, nonlincons, options);
-                            
+                        [dw, ~, obj.exitFlag] = fmincon(@(dw)cfMVT_wTC(obj, dw, obj.curWts', ...
+                                            obj.benWts', obj.expRts, zeros(size(obj.tnsCst)),  ...
+                                    obj.lambdaAbs, obj.lambdaRel, obj.covMtx), dw0, A, b, Aeq, ... 
+                                    beq, lb, ub, nonlincons, options);
                         obj.optWts = obj.curWts + (dw(1:end/2) - dw(end/2+1:end))';
-                        obj.funcValCur = cfMVT(obj.curWts', obj.benWts', obj.expRts, obj.lambdaAbs, ...
-                                            obj.lambdaRel, obj.covMtx);
-                        obj.funcValOpt = cfMVT(obj.optWts', obj.benWts', obj.expRts, obj.lambdaAbs, ...
-                                            obj.lambdaRel, obj.covMtx);
+                        obj.funcValCur = cfMVT(obj, obj.curWts', obj.benWts', obj.expRts, ...
+                                            obj.lambdaAbs, obj.lambdaRel, obj.covMtx);
+                        obj.funcValOpt = cfMVT(obj, obj.optWts', obj.benWts', obj.expRts, ...
+                                            obj.lambdaAbs, obj.lambdaRel, obj.covMtx);
                     end
                 case 'QAprx' % We use MVT for simplicity MV = MVT with lambdaRel = 0
                     if ~getOptimal % Optimize with transaction costs and QAprx
-                        [dw, ~, obj.exitFlag] = fmincon(@(dw)cfMVT_wTC_wQAprx(dw, obj.curWts', obj.benWts', ...
-                                           obj.optWts', obj.expRts, obj.tnsCst, obj.lambdaAbs, ... 
-                                            obj.lambdaRel, obj.covMtx, obj.QAprx), ...
-                                    dw0, A, b, Aeq, beq, lb, ub, nonlincons, options);
+                        [dw, ~, obj.exitFlag] = fmincon(@(dw)cfMVT_wTC_wQAprx(obj, dw, ...
+                                           obj.curWts', obj.benWts', obj.optWts', obj.expRts, ... 
+                                           obj.tnsCst, obj.lambdaAbs, obj.lambdaRel, obj.covMtx, ...
+                                    obj.QAprx), dw0, A, b, Aeq, beq, lb, ub, nonlincons, options);
                             
                         obj.newWts = obj.curWts + (dw(1:end/2) - dw(end/2+1:end))';
-                        obj.funcValCur = cfMVT(obj.curWts', obj.benWts', obj.expRts, obj.lambdaAbs, ...
-                                            obj.lambdaRel, obj.covMtx);
-                        obj.funcValNew = cfMVT(obj.newWts', obj.benWts', obj.expRts, obj.lambdaAbs, ...
-                                            obj.lambdaRel, obj.covMtx);
+                        obj.funcValCur = cfMVT(obj, obj.curWts', obj.benWts', obj.expRts, ...
+                                            obj.lambdaAbs, obj.lambdaRel, obj.covMtx);
+                        obj.funcValNew = cfMVT(obj, obj.newWts', obj.benWts', obj.expRts, ...
+                                            obj.lambdaAbs, obj.lambdaRel, obj.covMtx);
                         obj.paidTC = obj.tnsCst * abs(obj.curWts - obj.newWts)';
                     else % Optimize without transaction costs and without QAprx
-                        [dw, ~, obj.exitFlag] = fmincon(@(dw)cfMVT_wTC_wQAprx(dw, obj.curWts', obj.benWts', ...
-                                            zeros(1,numAssets)', obj.expRts, zeros(size(obj.tnsCst)), obj.lambdaAbs,  ...
-                                            obj.lambdaRel, obj.covMtx, zeros(size(obj.QAprx))), ...
-                                    dw0, A, b, Aeq, beq, lb, ub, nonlincons, options);
-                            
+                        [dw, ~, obj.exitFlag] = fmincon(@(dw)cfMVT_wTC_wQAprx(obj, dw, ...
+                                           obj.curWts', obj.benWts', obj.optWts', obj.expRts, ... 
+                                           zeros(size(obj.tnsCst)), obj.lambdaAbs, obj.lambdaRel, ...
+                                    obj.covMtx, zeros(size(obj.QAprx))), dw0, A, b, Aeq, beq, ...
+                                    lb, ub, nonlincons, options);
                         obj.optWts = obj.curWts + (dw(1:end/2) - dw(end/2+1:end))';
-                        obj.funcValCur = cfMVT(obj.curWts', obj.benWts', obj.expRts, obj.lambdaAbs, ...
-                                            obj.lambdaRel, obj.covMtx);
-                        obj.funcValOpt = cfMVT(obj.optWts', obj.benWts', obj.expRts, obj.lambdaAbs, ...
-                                            obj.lambdaRel, obj.covMtx);
+                        obj.funcValCur = cfMVT(obj, obj.curWts', obj.benWts', obj.expRts, ...
+                                            obj.lambdaAbs, obj.lambdaRel, obj.covMtx);
+                        obj.funcValOpt = cfMVT(obj, obj.optWts', obj.benWts', obj.expRts, ...
+                                            obj.lambdaAbs, obj.lambdaRel, obj.covMtx);
                     
                     end
             end
@@ -139,6 +140,38 @@ classdef Costfunction < Portfolio & matlab.mixin.CustomDisplay
             end
             
         end % optimizeCostfunction
+        
+        % Mean-variance-tracking
+        function f = cfMVT(obj, w, wb,  mu, lambda_abs, lambda_rel, cov_matrix)
+
+            f = -mu*w + lambda_abs*w'*cov_matrix*w + lambda_rel*(w - wb)' * cov_matrix * (w - wb);
+
+        end
+        
+        % Mean-variance-tracking with tranaction costs
+        function f = cfMVT_wTC(obj, dw, wc, wb, mu, tnsCst, lambdaAbs,  lambdaRel, covMtx)
+
+            % Split weights into positive and negative parts
+            u = dw(1:end/2);
+            v = dw(end/2+1:end);
+
+            f = -mu*(wc +(u - v)) + lambdaAbs*(wc + (u - v))'*covMtx*(wc + (u - v)) + ...
+                + lambdaRel*(wc - wb + (u - v))'*covMtx*(wc - wb +(u - v)) + tnsCst*(u + v);
+
+        end % cfMVT_wTC
+
+        % Mean-variance-tracking with tranaction costs
+        function f = cfMVT_wTC_wQAprx(obj, dw, wc, wb, wo, mu, tnsCst, lambdaAbs,  lambdaRel, covMtx, QAprx)
+
+            % Split weights into positive and negative parts
+            u = dw(1:end/2);
+            v = dw(end/2+1:end);
+
+            f = -mu*(wc +(u - v)) + lambdaAbs*(wc + (u - v))'*covMtx*(wc + (u - v)) + ...
+                + lambdaRel*(wc - wb + (u - v))'*covMtx*(wc - wb +(u - v)) + tnsCst*(u + v) + ...
+                ((wc + (u - v)) - wo)'*QAprx*((wc + (u - v)) - wo);
+
+        end 
         
     end
     methods (Access = protected)
@@ -164,55 +197,4 @@ classdef Costfunction < Portfolio & matlab.mixin.CustomDisplay
     
 end % classdef Costfunc
 
-% Actual costfunctions
-% Mean-variance error
-function f = cfMV(w, mu, lambda_abs,  cov_matrix)
 
-    f = -mu*w + lambda_abs*w'*cov_matrix*w;
-
-end
-
-% Mean-variance-tracking
-function f = cfMVT(w, wb,  mu, lambda_abs, lambda_rel, cov_matrix)
-
-    f = -mu*w + lambda_abs*w'*cov_matrix*w + lambda_rel*(w - wb)' * cov_matrix * (w - wb);
-
-end
-
-
-% Mean-variance with tranaction costs
-function f = cfMV_wTC(dw, wc, mu, tnsCst, lambdaAbs,  covMtx)
-
-    % Split weights into positive and negative parts
-    u = dw(1:end/2);
-    v = dw(end/2+1:end);
-
-    f = -mu*(wc +(u - v)) + lambdaAbs*(wc + (u - v))'*covMtx*(wc + (u - v)) + ...
-        + tnsCst*(u + v);
-
-end % cfMVT_wTC
-
-% Mean-variance-tracking with tranaction costs
-function f = cfMVT_wTC(dw, wc, wb, mu, tnsCst, lambdaAbs,  lambdaRel, covMtx)
-
-    % Split weights into positive and negative parts
-    u = dw(1:end/2);
-    v = dw(end/2+1:end);
-
-    f = -mu*(wc +(u - v)) + lambdaAbs*(wc + (u - v))'*covMtx*(wc + (u - v)) + ...
-        + lambdaRel*(wc - wb + (u - v))'*covMtx*(wc - wb +(u - v)) + tnsCst*(u + v);
-
-end % cfMVT_wTC
-
-% Mean-variance-tracking with tranaction costs
-function f = cfMVT_wTC_wQAprx(dw, wc, wb, wo, mu, tnsCst, lambdaAbs,  lambdaRel, covMtx, QAprx)
-
-    % Split weights into positive and negative parts
-    u = dw(1:end/2);
-    v = dw(end/2+1:end);
-
-    f = -mu*(wc +(u - v)) + lambdaAbs*(wc + (u - v))'*covMtx*(wc + (u - v)) + ...
-        + lambdaRel*(wc - wb + (u - v))'*covMtx*(wc - wb +(u - v)) + tnsCst*(u + v) + ...
-        ((wc + (u - v)) - wo)'*QAprx*((wc + (u - v)) - wo);
-
-end
--- testOptReb.m
+++ testOptReb.m
@@ -35,7 +35,7 @@ mvd = optimizeCostfunction(mvd, 0);
 %% MvD - Approximation of best multiperiod rebalancing solution
 simFreq = 12;
 numPeriods = 36;
-numSims = 10;
+numSims = 400;
 
 tic
 fprintf('Training/Calibration  - MvD - Quadratic Approximation ... \n')
@@ -73,7 +73,7 @@ indxBest = find(totCost == min(totCost));
 bestMvD = mvd(1 + indxBest);
 
 % We need a greater number of simulations
-numSims = 50;
+numSims = 3000;
 propMap = containers.Map({'numSims'}, {numSims});
 bestMvD = setProperties(bestMvD, propMap);
 
